2c09d9fd65f9a378bb8b636453d02db6eba9ac6c
asmaaabdelsattar <asmaa.abdelsattarr@gmail.com>
2024-08-17 17:50:01 +0300

    starting point

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+ * File:   main.c
+ * Author: asmaa
+ *
+ * Created on August 17, 2024, 4:30 PM
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
